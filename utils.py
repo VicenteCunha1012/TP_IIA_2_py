51ca1c8ccbc1f2554810227d9b6fb37219ad7dba
Vicente Cunha <vicentenmc@gmail.com>
2023-11-20 19:55:11 +0000

    correcao no readFile

--- utils.py
+++ utils.py
@@ -1,14 +1,13 @@
 def readFile(nome, GraphModel):
     for line in open(nome, "r"):
-
+        values = line.split(" ")
         if line[0] == "k":
-            GraphModel.k = int(line[2])
+            GraphModel.k = int(values[1])
         elif line[0:6] == "p edge":
-            GraphModel.vertices = int(line[7])
-            GraphModel.arestas = int(line[9])
+            GraphModel.vertices = int(values[2])
+            GraphModel.arestas = int(values[3])
             GraphModel.fillGraph()
         elif line[0] == "e":
-            values = line.split(" ")
             x = int(values[1]) - 1
             y = int(values[2]) - 1
             content = int(values[3].strip("\n"))
