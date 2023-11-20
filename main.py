859e0cf7ae0aecdcf49ee195ac8bcda65add1ca3
Tomas Ramos <tramoscont@gmail.com>
2023-11-20 22:09:45 +0000

    olee

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from algo import *
 from random import randint
 import time
 
+
 class GraphModel:
-    def __init__(self, k=0, vertices=0, arestas=0, graph=[], solucao=[], cost=0):
-        self.k = k
-        self.vertices = vertices
-        self.arestas = arestas
-        self.graph = graph
-        self.solucao = solucao
-        self.cost = cost
+    def __init__(self):
+        self.k = 0
+        self.vertices = 0
+        self.arestas = 0
+        self.graph = [[]]
+        self.solucao = []
+        self.cost = 0
+        self.path = ""
 
     def fillGraph(self):
         self.graph = [[0] * self.vertices for _ in range(self.vertices)]
@@ -38,42 +40,53 @@ class GraphModel:
     def getCost(self):
         cost = 0
         for i in range(self.vertices):
-            currentCost = 0
-            if (self.solucao[i] != 0):
-                for j in range(self.vertices):
-                    if (self.solucao[j] != 0):
-                        currentCost += self.graph[i][j]
-                if (currentCost == 0):
+            if self.solucao[i] != 0:
+                currentCost = sum(self.graph[i][j] for j in range(self.vertices) if self.solucao[j] != 0)
+                if currentCost == 0:
                     return -1
                 cost += currentCost
-        return int(cost / 2)
+
+        return cost // 2
 
     def createInitialSolution(self):
+        start = time.time()
         self.cost = -1;
         while self.cost == -1:
             self.generateRandomSol()
             self.cost = self.getCost()
 
+        end = time.time()
+        return "{:.5f}".format(end - start)
+
+    def initModel(self):
+        folder = input("Introduza a pasta (vazio se estiver na mesma)\n--> ")
+        file = input("Introduza o nome do ficheiro\n-->")
+        numOfRuns = input("Introduza o numero de iteracoes\n-->")
+        self.path = folder + "/" + file
+        if (self.path == "/" and numOfRuns == ""):
+            self.path = "text files/test.txt"
+            numOfRuns = 10
+        start = time.time()
+        readFile(self.path, self)
+        end = time.time()
+        return "{:.5f}".format(end - start)
+
+
 
 if __name__ == "__main__":
-
-    folder = input("Introduza a pasta (vazio se estiver na mesma)\n--> ")
-    file = input("Introduza o nome do ficheiro\n-->")
-    numOfRuns = input("Introduza o numero de iteracoes\n-->")
-    path = folder + "/" + file
     model = GraphModel()
-    if(path == "/" and numOfRuns == ""):
-        path = "text files/test.txt"
-        numOfRuns = 10
-    start = time.time()
-    readFile(path, model)
+    timeInit = model.initModel()
     model.printGraph()
-    print(model.k)
-    print(model.vertices)
-    print(model.arestas)
-    model.createInitialSolution()
-    print("Initial Solution:", model.solucao)
-    print("Inital Cost:", model.cost)
-    end = time.time()
-    elapsed_time_str = "{:.5f}".format(end-start)
-    print("Elapsed time: "+ elapsed_time_str+ "seconds.")
+    print(f"Path: {model.path:>45}")
+    print(f"K: {model.k:>48}")
+    print(f"Vertex Number: {model.vertices:>36}")
+    print(f"Edge Number: {model.arestas:>38}")
+    timeCreateSol = model.createInitialSolution()
+    print(f"Inital Cost: {model.cost:>38}")
+    print(f"Initialization Time: {timeInit:>21} seconds.")
+    print(f"Initial Solution Creation Time: {timeCreateSol:>10} seconds.")
+    totalTime = float(timeInit) + float(timeCreateSol)
+    print(f"Time Elapsed: {str(totalTime):>28} seconds.")
+    print(f"Initial Solution: {str(model.solucao):>33}")
+
+
